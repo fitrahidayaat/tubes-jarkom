5a6f29c239c66d5898ffad7b3ed445371d49a544
Fitra Hidayat <99539325+fitrahidyt@users.noreply.github.com>
2023-05-07 20:00:37 +0700

    change html and fix response

--- tcp_socket_ver1.py
+++ tcp_socket_ver1.py
@@ -18,21 +18,20 @@ while True:
     connectionSocket, addr = serverSocket.accept() 
     try: 
         message = connectionSocket.recv(1024).decode()   
-        filename = message.split(' ')[1]             
+        filename = message.split(' ')[1]    
         f = open(filename[1:])                     
-        outputdata = f.read() 
+        outputdata = f.read()          
         #Send one HTTP header line into socket 
- 
-        connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
+        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+        connectionSocket.send(response.encode())
    
         #Send the content of the requested file to the client 
         connectionSocket.send(outputdata.encode())
-        connectionSocket.send("\r\n".encode()) 
         connectionSocket.close() 
     except IOError: 
         #Send response message for file not found 
      
-        connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
+        connectionSocket.send('HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>404 Not Found</h1>'.encode())
 
         #Close client socket 
         connectionSocket.close()

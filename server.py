6d2f261d5bee8c3cf0641d42fc90371648f23760
Fitra Hidayat <99539325+fitrahidyt@users.noreply.github.com>
2023-05-28 23:06:50 +0700

    update html

--- server.py
+++ server.py
@@ -1,34 +1,51 @@
 #import socket module
 from socket import * 
 import sys 
+
 # In order to terminate the program 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-# Prepare a sever socket 
-# Fill in start 
+# Prepare a server socket 
 serverPort = 8080
 serverSocket.bind(('localhost', serverPort))
 serverSocket.listen(1)
 print ('the web server is up on port:',serverPort)
-# Fill in end 
+
+# Geting matched content type
+def get_content_type(filename):
+    content_type = filename.split('.')[-1]
+    mp = {
+        'image' : ['jpg', 'png', 'ico', 'jpeg', 'gif', 'svg+xml', 'tiff', 'webp', 'bmp', 'x-icon'],
+        'text' : ['html', 'css', 'js'],
+        'application' : ['pdf', 'json', 'xml', 'zip', 'gzip', 'x-tar', 'x-rar-compressed', 'x-7z-compressed', 'x-bzip', 'x-bzip2', 'x-rar', 'x-tar', 'x-zip-compressed', 'x-zip', 'x-zip-compr'],
+        'audio' : ['mpeg', 'ogg', 'wav', 'webm', 'x-aac', 'x-flac', 'x-ma'],
+        'video' : ['mpeg', 'mp4', 'quicktime', 'x-msvideo', 'webm'],
+        'multipart' : ['mixed', 'alternative', 'related', 'form-data', 'signed', 'encrypted']
+    }
+    for key, value in mp.items():
+        if content_type in value:
+            return key + '/' + content_type
+    
 
 while True: 
     #Establish the connection 
     print('Ready to serve...') 
     connectionSocket, addr = serverSocket.accept() 
     try: 
+        # Receives the request message from the client
         message = connectionSocket.recv(1024).decode()   
-        filename = message.split(' ')[1]    
-        f = open(filename[1:])                     
+        filename = message.split(' ')[1] if message.split(' ')[0] == 'GET' else ''
+        f = open(filename[1:], 'rb')                   
         outputdata = f.read()        
           
         #Send one HTTP header line into socket 
-        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+        response = 'HTTP/1.1 200 OK\r\n'
+        response += f'Content-Type: {get_content_type(filename)}\r\n'
+        response += '\r\n'
         connectionSocket.send(response.encode())
    
         #Send the content of the requested file to the client 
-        connectionSocket.send(outputdata.encode())
-
+        connectionSocket.sendall(outputdata)
         #Close client socket
         connectionSocket.close() 
     except IOError: 
